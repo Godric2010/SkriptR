80ec9a5f05794790a3d337a80e684e06489d491c
Sebastian <sebastian.borsch1996@gmail.com>
2023-03-05 10:26:07 +0100

    Added first 3D primitive

--- resa-renderer/src/mesh.rs
+++ resa-renderer/src/mesh.rs
@@ -24,6 +24,29 @@ impl Hash for Mesh {
 	}
 }
 
+
+pub fn create_primitive_cube() -> Mesh {
+	let vertices = vec![
+		Vertex { position: Vector3::new(-0.5, -0.5, 0.5), uv: Vector2::new(0.0, 0.0) },
+		Vertex { position: Vector3::new(0.5, -0.5, 0.5), uv: Vector2::new(0.0, 0.0) },
+		Vertex { position: Vector3::new(-0.5, 0.5, 0.5), uv: Vector2::new(0.0, 0.0) },
+		Vertex { position: Vector3::new(0.5, 0.5, 0.5), uv: Vector2::new(0.0, 0.0) },
+		Vertex { position: Vector3::new(-0.5, -0.5, -0.5), uv: Vector2::new(0.0, 0.0) },
+		Vertex { position: Vector3::new(0.5, -0.5, -0.5), uv: Vector2::new(0.0, 0.0) },
+		Vertex { position: Vector3::new(-0.5, 0.5, -0.5), uv: Vector2::new(0.0, 0.0) },
+		Vertex { position: Vector3::new(0.5, 0.5, -0.5), uv: Vector2::new(0.0, 0.0) },
+	];
+	let triangle_list = vec![
+		0, 2, 3, 0, 1, 3, //Front
+		4, 6, 7, 4, 5, 7, //Back
+		1, 3, 7, 1, 5, 7, // Right
+		0, 2, 6, 0, 4, 6, // Left
+		2, 6, 7, 2, 3, 7, // Top
+		0, 4, 5, 0, 1, 5, // Bottom
+	];
+	Mesh::new(vertices, triangle_list)
+}
+
 pub fn create_primitive_quad() -> Mesh {
 	let vertices = vec![
 		Vertex { position: Vector3::new(-1.0, -1.0, 0.0), uv: Vector2::new(0.0, 0.0) },
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 extern crate core;
 
 use std::rc::Rc;
-use resa_renderer::mesh::{create_primitive_quad, create_primitive_triangle};
+use resa_renderer::mesh::{create_primitive_cube, create_primitive_quad, create_primitive_triangle};
 
 use rendering::camera::Camera;
 use rendering::transform::Transform;
@@ -29,14 +29,21 @@ fn main() {
 	let material = Material {
 		shader_id: 1,
 		pipeline_type: PipelineType::Opaque,
-		color: Color::new(255,0,0,255),
+		color: Color::new(255, 0, 0, 255),
 	};
 
-	let material02 = Material{
+	let material02 = Material {
 		shader_id: 1,
 		pipeline_type: PipelineType::Opaque,
-		color: Color::new(0,0,0,255	),
+		color: Color::new(0, 0, 0, 255),
 	};
+
+	let material03 = Material {
+		shader_id: 1,
+		pipeline_type: PipelineType::Opaque,
+		color: Color::new(68, 204, 0, 255),
+	};
+
 	let entity01 = world.borrow_mut().new_entity();
 	let transform = Transform { position: [0., 0., 0.0], angle: 0.0, scale: 1.0 };
 	let mut mesh_renderer = app.rendering.load_mesh(create_primitive_triangle());
@@ -55,13 +62,16 @@ fn main() {
 	let entity02 = world.borrow_mut().new_entity();
 	let transform = Transform { position: [0.8, 0.2, 0.0], angle: 0.0, scale: 0.2 };
 	let mut mesh_renderer = app.rendering.load_mesh(create_primitive_quad());
-
-
-
 	app.rendering.assign_material_to_mesh(&mut mesh_renderer, material02.clone());
-
 	world.borrow_mut().add_component(&entity02, transform);
 	world.borrow_mut().add_component(&entity02, mesh_renderer);
+
+	let entity04 = world.borrow_mut().new_entity();
+	let transform = Transform { position: [-1.1, 1.0, 0.2], angle: 0.3, scale: 1.0 };
+	let mut mesh_renderer = app.rendering.load_mesh(create_primitive_cube());
+	app.rendering.assign_material_to_mesh(&mut mesh_renderer, material03.clone());
+	world.borrow_mut().add_component(&entity04, transform);
+	world.borrow_mut().add_component(&entity04, mesh_renderer);
 
 	app.run_window_loop();
 }
